458ca2d815fab049be6b56a48b2b1c93dad40575
Etienne Lessard <elessard@avencall.com>
2012-06-20 13:05:55 -0400

    remove AGI from dird.

--- xivo-dird/xivo_dird/bin/xivo_dird.py
+++ xivo-dird/xivo_dird/bin/xivo_dird.py
@@ -2,9 +2,9 @@
 
 # Copyright (C) 2012  Avencall
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -12,20 +12,18 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from __future__ import absolute_import
 
-import logging
-from xivo_dird.dird_server import DirdServer
-from xivo import daemonize
 import argparse
+import logging
 import sys
+from xivo import daemonize
+from xivo_dird.dird_server import DirdServer
 
 DAEMONNAME = 'xivo-dird'
-DEBUG_MODE = False
 LOGFILENAME = '/var/log/%s.log' % DAEMONNAME
 PIDFILE = '/var/run/%s.pid' % DAEMONNAME
 
--- xivo-dird/xivo_dird/dird_server.py
+++ xivo-dird/xivo_dird/dird_server.py
@@ -1,20 +1,26 @@
 # -*- coding: UTF-8 -*-
 
-from __future__ import absolute_import
+# Copyright (C) 2012  Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import logging
-from xivo_dird.agi.agi_server import AgiServer
 
 logger = logging.getLogger(__name__)
 
 
 class DirdServer(object):
-
-    def __init__(self):
-        pass
-
     def run(self):
         logger.info('directory server starting ....')
-        agi_server = AgiServer()
-        agi_server.run()
         logger.info('directory server stopped .....')
