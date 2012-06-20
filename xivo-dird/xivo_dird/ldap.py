308f7376b2053e2bc5bde7ed76b37ddbd6f6453f
Etienne Lessard <elessard@avencall.com>
2012-06-20 14:19:55 -0400

    update for packaging-farm.

--- xivo-dird/xivo_dird/ldap.py
+++ xivo-dird/xivo_dird/ldap.py
@@ -1,16 +1,11 @@
 # XiVO CTI Server
 
-__copyright__ = 'Copyright (C) 2007-2012  Avencall'
-
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
+# Copyright (C) 2007-2012  Avencall
 #
-# Alternatively, XiVO CTI Server is available under other licenses directly
-# contracted with Pro-formatique SARL. See the LICENSE file at top of the
-# source tree or delivered in the installable package in which XiVO CTI Server
-# is distributed for more details.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,7 +13,7 @@ __copyright__ = 'Copyright (C) 2007-2012  Avencall'
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 import ldap
 import logging
