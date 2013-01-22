c092c50ddea91881b92c54862c0c5dce32455a65
Gregory Eric Sanderson <gsanderson@avencall.com>
2013-01-22 16:29:56 -0500

    adjust license in python file headers

--- xivo-dird/xivo_dird/dird_server.py
+++ xivo-dird/xivo_dird/dird_server.py
@@ -1,6 +1,6 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
-# Copyright (C) 2012  Avencall
+# Copyright (C) 2012-2013 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
