4967312bd9502524b06548d615c0a25b154dc49c
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2014-01-08 10:09:56 -0500

    update copyright years

--- xivo-dird/xivo_dird/__init__.py
+++ xivo-dird/xivo_dird/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2013 Avencall
+# Copyright (C) 2013-2014 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,4 +14,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
-
