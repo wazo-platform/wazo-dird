830aae32a182fec8cb1650ac4aa612104f6c382c
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-05-20 09:02:39 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import fnmatch
+import os
+
 from distutils.core import setup
+
+
+def is_package(path):
+    is_svn_dir = fnmatch.fnmatch(path, '*/.svn*')
+    is_test_module = fnmatch.fnmatch(path, '*tests')
+    return not (is_svn_dir or is_test_module)
+
+packages = [p for p, _, _ in os.walk('xivo_dird') if is_package(p)]
+
 
 setup(
     name='xivo-dird',
@@ -9,10 +21,7 @@ setup(
     description='XiVO Directory Daemon',
     author='Avencall',
     author_email='dev@avencall.com',
-    url='http://git.xivo.fr/',
-    packages=[
-        'xivo_dird',
-        'xivo_dird.directory',
-        'xivo_dird.directory.data_sources',
-    ],
+    url='https://github.com/xivo-pbx/xivo-dird',
+    packages=packages,
+    scripts=['bin/xivo-dird'],
 )
