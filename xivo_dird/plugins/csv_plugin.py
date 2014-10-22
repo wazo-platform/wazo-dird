95f938950493bc63fbefeb6f5574cef8da9a3c99
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2014-10-22 13:14:13 -0400

    fix the directory lookup to match the documentation in csv_plugin.load

--- xivo_dird/plugins/csv_plugin.py
+++ xivo_dird/plugins/csv_plugin.py
@@ -51,7 +51,7 @@ class CSVPlugin(BaseSourcePlugin):
         self._load_file()
         self._SourceResult = make_result_class(self._name,
                                                self._config.get(self.UNIQUE_COLUMNS, []),
-                                               self._config.get('source_to_display_columns', {}))
+                                               self._config.get('source_to_display', {}))
 
     def name(self):
         return self._name
