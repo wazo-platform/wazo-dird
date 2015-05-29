02b360b7da4a16d99c9f868b46d00561cfcc90da
Sebastien Duthil <sduthil@avencall.com>
2015-05-29 13:58:29 -0400

    replace /doc with /api in URL

--- xivo_dird/swagger/resource.py
+++ xivo_dird/swagger/resource.py
@@ -7,7 +7,7 @@ class SwaggerResource(Resource):
 
     api_package = "xivo_dird.swagger"
     api_filename = "api.json"
-    api_path = "/doc/api.json"
+    api_path = "/api/api.json"
 
     @classmethod
     def add_resource(cls, api):
