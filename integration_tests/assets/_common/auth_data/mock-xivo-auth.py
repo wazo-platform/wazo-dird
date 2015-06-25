4c3a4bca46e75c1b542f3ef7ff6cd1e1806638ef
Sebastien Duthil <sduthil@avencall.com>
2015-06-25 09:45:05 -0400

    use HTTPS for mock-xivo-auth

--- integration_tests/assets/_common/auth_data/mock-xivo-auth.py
+++ integration_tests/assets/_common/auth_data/mock-xivo-auth.py
@@ -1,10 +1,15 @@
 import sys
 
 from flask import Flask
+from OpenSSL import SSL
 
 app = Flask(__name__)
 
 port = int(sys.argv[1])
+
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('/etc/ssl/server.key')
+context.use_certificate_file('/etc/ssl/server.crt')
 
 
 @app.route("/0.1/token/valid-token", methods=['HEAD'])
@@ -12,4 +17,4 @@ def token_head():
     return '', 204
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, ssl_context=context)
