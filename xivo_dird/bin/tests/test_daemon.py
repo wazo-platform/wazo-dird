d2e58ababac5f07ca7de56fe87960358c3f0a959
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-08-12 13:55:25 -0400

    test_daemon fix the patches
    daemon.py now import xivo_logging instead of setup_logging

--- xivo_dird/bin/tests/test_daemon.py
+++ xivo_dird/bin/tests/test_daemon.py
@@ -24,24 +24,24 @@ from xivo_dird.bin import daemon
 
 @patch('xivo_dird.bin.daemon.pidfile_context')
 @patch('xivo_dird.bin.daemon.change_user')
-@patch('xivo_dird.bin.daemon.setup_logging')
+@patch('xivo_dird.bin.daemon.xivo_logging')
 @patch('xivo_dird.bin.daemon.Controller')
 @patch('xivo_dird.bin.daemon.load_config')
 class TestXivoDird(TestCase):
 
-    def test_main_injects_argv_into_config_loading(self, load_config, controller_init, setup_logging, change_user, pidfile_context):
+    def test_main_injects_argv_into_config_loading(self, load_config, controller_init, xivo_logging, change_user, pidfile_context):
         daemon.main(s.argv)
 
         load_config.assert_called_once_with(ANY, s.argv)
 
-    def test_main_injects_config_in_controller(self, load_config, controller_init, setup_logging, change_user, pidfile_context):
+    def test_main_injects_config_in_controller(self, load_config, controller_init, xivo_logging, change_user, pidfile_context):
         config = load_config.return_value
 
         daemon.main(s.argv)
 
         controller_init.assert_called_once_with(config)
 
-    def test_main_setup_logging(self, load_config, controller_init, setup_logging, change_user, pidfile_context):
+    def test_main_setup_logging(self, load_config, controller_init, xivo_logging, change_user, pidfile_context):
         load_config.return_value = {
             'debug': s.debug,
             'log_filename': s.log_filename,
@@ -53,9 +53,9 @@ class TestXivoDird(TestCase):
 
         daemon.main(s.argv)
 
-        setup_logging.assert_called_once_with(s.log_filename, s.foreground, s.debug, s.log_level)
+        xivo_logging.setup_logging.assert_called_once_with(s.log_filename, s.foreground, s.debug, s.log_level)
 
-    def test_main_when_config_user_then_change_user(self, load_config, controller_init, setup_logging, change_user, pidfile_context):
+    def test_main_when_config_user_then_change_user(self, load_config, controller_init, xivo_logging, change_user, pidfile_context):
         load_config.return_value = {
             'debug': s.debug,
             'log_filename': s.log_filename,
@@ -69,7 +69,7 @@ class TestXivoDird(TestCase):
 
         change_user.assert_called_once_with(s.user)
 
-    def test_main_when_no_config_user_then_dont_change_user(self, load_config, controller_init, setup_logging, change_user, pidfile_context):
+    def test_main_when_no_config_user_then_dont_change_user(self, load_config, controller_init, xivo_logging, change_user, pidfile_context):
         load_config.return_value = {
             'debug': s.debug,
             'log_filename': s.log_filename,
@@ -83,7 +83,7 @@ class TestXivoDird(TestCase):
 
         assert_that(change_user.call_count, equal_to(0))
 
-    def test_main_calls_controller_run(self, load_config, controller_init, setup_logging, change_user, pidfile_context):
+    def test_main_calls_controller_run(self, load_config, controller_init, xivo_logging, change_user, pidfile_context):
         controller = controller_init.return_value
         load_config.return_value = {
             'debug': s.debug,
