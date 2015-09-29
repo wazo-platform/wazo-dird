f79999b89408891abb6b7adae563a8f880077781
François Blackburn <fblackburn@avencall.com>
2015-09-29 09:01:54 -0400

    add aastra,thomson,polycom,snom,yealink plugins

--- xivo_dird/plugins/aastra_view.py
+++ xivo_dird/plugins/aastra_view.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>
 
+
 import os
 import logging
 
@@ -24,41 +25,35 @@ from jinja2 import Environment
 from xivo_dird import BaseViewPlugin
 from xivo_dird.core.rest_api import api
 from xivo_dird.plugins.phone_helpers import new_phone_display_from_config
-from xivo_dird.plugins.phone_view import PhoneMenu, PhoneInput, PhoneLookup
+from xivo_dird.plugins.phone_view import PhoneInput, PhoneLookup
 
 logger = logging.getLogger(__name__)
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_FOLDER = os.path.join(CURRENT_PATH, 'lookup_templates')
-TEMPLATE_CISCO_MENU = "cisco_menu.jinja"
-TEMPLATE_CISCO_INPUT = "cisco_input.jinja"
-TEMPLATE_CISCO_RESULTS = "cisco_results.jinja"
+TEMPLATE_AASTRA_INPUT = "aastra_input.jinja"
+TEMPLATE_AASTRA_RESULTS = "aastra_results.jinja"
 
 CONTENT_TYPE = 'text/xml'
 MAX_ITEM_PER_PAGE = 16
 
 
-class CiscoViewPlugin(BaseViewPlugin):
+class AastraViewPlugin(BaseViewPlugin):
 
-    cisco_menu = '/directories/menu/<profile>/cisco'
-    cisco_input = '/directories/input/<profile>/cisco'
-    cisco_lookup = '/directories/lookup/<profile>/cisco'
+    aastra_input = '/directories/input/<profile>/aastra'
+    aastra_lookup = '/directories/lookup/<profile>/aastra'
 
     def load(self, args=None):
         phone_display = new_phone_display_from_config(args['config'])
         jinja_env = Environment(loader=FileSystemLoader(TEMPLATE_FOLDER))
-        template_menu = jinja_env.get_template(TEMPLATE_CISCO_MENU)
-        template_input = jinja_env.get_template(TEMPLATE_CISCO_INPUT)
-        template_lookup = jinja_env.get_template(TEMPLATE_CISCO_RESULTS)
+        template_input = jinja_env.get_template(TEMPLATE_AASTRA_INPUT)
+        template_lookup = jinja_env.get_template(TEMPLATE_AASTRA_RESULTS)
 
         lookup_service = args['services'].get('lookup')
         if lookup_service:
-            PhoneMenu.configure(lookup_service)
             PhoneInput.configure(lookup_service)
             PhoneLookup.configure(lookup_service, phone_display)
-            api.add_resource(PhoneMenu, self.cisco_menu, endpoint='CiscoPhoneMenu',
-                             resource_class_args=(template_menu, CONTENT_TYPE))
-            api.add_resource(PhoneInput, self.cisco_input, endpoint='CiscoPhoneInput',
+            api.add_resource(PhoneInput, self.aastra_input, endpoint='AastraPhoneInput',
                              resource_class_args=(template_input, CONTENT_TYPE))
-            api.add_resource(PhoneLookup, self.cisco_lookup, endpoint='CiscoPhoneLookup',
+            api.add_resource(PhoneLookup, self.aastra_lookup, endpoint='AastraPhoneLookup',
                              resource_class_args=(template_lookup, CONTENT_TYPE, MAX_ITEM_PER_PAGE))
---
+++ xivo_dird/plugins/phone_view.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
+import logging
+
+from flask import request
+from flask import Response
+from flask_restful import reqparse
+from time import time
+
+from xivo_dird.core import auth
+from xivo_dird.core.auth import AuthResource
+
+logger = logging.getLogger(__name__)
+
+
+def _error(code, msg):
+    return {'reason': [msg],
+            'timestamp': [time()],
+            'status_code': code}, code
+
+
+class PhoneMenu(AuthResource):
+
+    lookup_service = None
+
+    @classmethod
+    def configure(cls, lookup_service):
+        cls.lookup_service = lookup_service
+
+    def __init__(self, template, content_type):
+        self.template = template
+        self.content_type = content_type
+
+    def get(self, profile):
+        proxy_url = request.headers.get('Proxy-URL', None)
+        if not proxy_url:
+            proxy_url = request.base_url.replace('menu', 'input', 1)
+
+        token = request.headers['X-Auth-Token']
+        token_infos = auth.client().token.get(token)
+        xivo_user_uuid = token_infos['xivo_user_uuid']
+
+        context = {'xivo_proxy_url': proxy_url,
+                   'xivo_user_uuid': xivo_user_uuid}
+        response_xml = self.template.render(context)
+
+        return Response(response_xml, content_type=self.content_type, status=200)
+
+
+class PhoneInput(AuthResource):
+
+    lookup_service = None
+
+    @classmethod
+    def configure(cls, lookup_service):
+        cls.lookup_service = lookup_service
+
+    def __init__(self, template, content_type):
+        self.template = template
+        self.content_type = content_type
+
+    def get(self, profile):
+        proxy_url = request.headers.get('Proxy-URL', None)
+        if not proxy_url:
+            proxy_url = request.base_url.replace('input', 'lookup', 1)
+
+        token = request.headers['X-Auth-Token']
+        token_infos = auth.client().token.get(token)
+        xivo_user_uuid = token_infos['xivo_user_uuid']
+
+        context = {'xivo_proxy_url': proxy_url,
+                   'xivo_user_uuid': xivo_user_uuid}
+        response_xml = self.template.render(context)
+
+        return Response(response_xml, content_type=self.content_type, status=200)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('limit', type=int, required=False, help='limit cannot be converted', location='args')
+parser.add_argument('offset', type=int, required=False, help='offset cannot be converted', location='args')
+parser.add_argument('term', type=unicode, required=True, help='term is missing', location='args')
+
+
+class PhoneLookup(AuthResource):
+
+    lookup_service = None
+    phone_display = None
+
+    @classmethod
+    def configure(cls, lookup_service, phone_display):
+        cls.lookup_service = lookup_service
+        cls.phone_display = phone_display
+
+    def __init__(self, template, content_type, max_item_per_page=None):
+        self.template = template
+        self.content_type = content_type
+        self.max_item_per_page = max_item_per_page
+
+    def get(self, profile):
+        proxy_url = request.headers.get('Proxy-URL', None)
+        if not proxy_url:
+            proxy_url = request.base_url
+
+        token = request.headers['X-Auth-Token']
+        token_infos = auth.client().token.get(token)
+        xivo_user_uuid = token_infos['xivo_user_uuid']
+
+        args = parser.parse_args()
+        term = args['term']
+        limit = self.max_item_per_page if args['limit'] is None else args['limit']
+        offset = 0 if args['offset'] is None else args['offset']
+
+        if limit < 0:
+            return _error(400, 'The limit should be positive')
+        if offset < 0:
+            return _error(400, 'The offset should be positive')
+
+        transform_func = self.phone_display.get_transform_function(profile)
+        results = self.lookup_service.lookup2(term, profile, args={}, token_infos=token_infos,
+                                              limit=limit, offset=offset, transform_func=transform_func)
+
+        context = {
+            'results': results['results'],
+            'xivo_proxy_url': proxy_url,
+            'xivo_user_uuid': xivo_user_uuid,
+            'term': term,
+            'limit': limit,
+            'offset_next': results['next_offset'],
+            'offset_previous': results['previous_offset']
+        }
+        response_xml = self.template.render(context)
+
+        return Response(response_xml, content_type=self.content_type, status=200)
