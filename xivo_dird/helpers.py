ba8313ba63faa560ef589a7d975caa8b7e1d5cd8
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-09-30 13:21:22 -0400

    Catch source exceptions
    This fix avoid getting no result if one of the source throws an exception

---
+++ xivo_dird/helpers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
+import logging
+
+
+logger = logging.getLogger()
+
+
+def no_throw_execute(default, f, *args, **kwargs):
+    try:
+        return f(*args, **kwargs)
+    except Exception:
+        logger.exception('An error occured in %s', f.__name__)
+    return default
--- xivo_dird/plugins/favorites_service.py
+++ xivo_dird/plugins/favorites_service.py
@@ -27,6 +27,7 @@ from contextlib import contextmanager
 from requests import RequestException
 
 from xivo_dird import BaseServicePlugin
+from xivo_dird import helpers
 from xivo_dird.core.consul import ls_from_consul
 
 logger = logging.getLogger(__name__)
@@ -80,7 +81,7 @@ class _FavoritesService(object):
         self._executor.shutdown()
 
     def _async_list(self, source, contact_ids, args):
-        future = self._executor.submit(source.list, contact_ids, args)
+        future = self._executor.submit(helpers.no_throw_execute, [], source.list, contact_ids, args)
         future.name = source.name
         return future
 
