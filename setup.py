a9a90231eb3c5a7dc6992b0c066a6179599bf7c2
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-10-08 12:27:29 -0400

    add babel to install_requires

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
 
     include_package_data=True,
     setup_requires=['babel'],
+    install_requires=['babel'],
     zip_safe=False,
     package_data={
         'xivo_dird.swagger': ['*.json']
