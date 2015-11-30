40850ba25adad851c99ed409df16f8e93447b831
François Blackburn <fblackburn@avencall.com>
2015-11-30 10:23:57 -0500

    add required acl to phone_view

--- integration_tests/assets/_common/auth_data/mock-xivo-auth.py
+++ integration_tests/assets/_common/auth_data/mock-xivo-auth.py
@@ -13,7 +13,14 @@ tokens = {'valid-token': 'uuid',
           'valid-token-1': 'uuid-1',
           'valid-token-2': 'uuid-2'}
 
-VALID_ACL = 'acl:dird.directories.reverse.default.uuid'
+VALID_ACLS = ['acl:dird.directories.reverse.default.uuid',
+              'acl:dird.directories.menu.default.uuid',
+              'acl:dird.directories.menu.menu.uuid',
+              'acl:dird.directories.input.default.uuid',
+              'acl:dird.directories.input.input.uuid',
+              'acl:dird.directories.lookup.default.uuid',
+              'acl:dird.directories.lookup.quiproquo.uuid',
+              ]
 
 
 @app.route("/0.1/token/valid-token", methods=['HEAD'])
@@ -23,7 +30,7 @@ def token_head():
     required_acl = request.args.get('scope', None)
     if required_acl is None:
         return '', 204
-    if required_acl == VALID_ACL:
+    if required_acl in VALID_ACLS:
         return '', 204
     return '', 401
 
