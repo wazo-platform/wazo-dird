3b385ef5d3b0fe06f5b094644f894aa0c528b870
François Blackburn <fblackburn@avencall.com>
2016-01-25 14:14:30 -0500

    unittest: remove debug print

--- xivo_dird/plugins/tests/test_ldap_plugin.py
+++ xivo_dird/plugins/tests/test_ldap_plugin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015 Avencall
+# Copyright (C) 2015-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -414,7 +414,6 @@ class TestLDAPConfig(unittest.TestCase):
             'unique_column_format': 'binary_uuid',
         })
         uids = [uuid]
-        print ldap_config.build_list_filter(uids)
 
         self.assertEqual('(objectGUID=%s)' % binary_uuid, ldap_config.build_list_filter(uids))
 
