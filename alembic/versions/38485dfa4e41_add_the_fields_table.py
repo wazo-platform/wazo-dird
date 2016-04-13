4991fc4f30b7cc6ac03e2f3bf06f1a111d650a39
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-04-13 13:27:29 -0400

    add the contact_fields table

---
+++ alembic/versions/38485dfa4e41_add_the_fields_table.py
@@ -0,0 +1,29 @@
+"""add the fields table
+
+Revision ID: 38485dfa4e41
+Revises: 265d68f9c337
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '38485dfa4e41'
+down_revision = '265d68f9c337'
+
+from alembic import op
+from sqlalchemy import Column, String, Text, Integer, ForeignKey
+
+table_name = 'dird_contact_fields'
+
+
+def upgrade():
+    op.create_table(
+        table_name,
+        Column('id', Integer, primary_key=True),
+        Column('name', String(20), nullable=False),
+        Column('value', Text()),
+        Column('contact_uuid', String(38), ForeignKey('dird_contact.uuid', ondelete='CASCADE'), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table(table_name)
