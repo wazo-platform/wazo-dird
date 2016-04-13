39c2a521f666191067d62c12b5f3d669fd5c44ea
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-04-13 10:58:36 -0400

    add the dird_user table

---
+++ alembic/versions/d237d33088c6_add_the_user_table.py
@@ -0,0 +1,25 @@
+"""add the user table
+
+Revision ID: d237d33088c6
+Revises: b0df0bc0ce37
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd237d33088c6'
+down_revision = 'b0df0bc0ce37'
+
+from alembic import op
+from sqlalchemy.schema import Column
+from sqlalchemy import String
+
+table_name = 'dird_user'
+
+
+def upgrade():
+    op.create_table(table_name,
+                    Column('xivo_user_uuid', String(38), nullable=False, primary_key=True))
+
+
+def downgrade():
+    op.drop_table(table_name)
