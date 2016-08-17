ed454e55b67b5a1782147888180e909e43ce5c0c
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-08-17 14:13:08 -0400

    refactor: rename the first arg of @classmethods cls

--- integration_tests/suite/base_dird_integration_test.py
+++ integration_tests/suite/base_dird_integration_test.py
@@ -48,7 +48,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
     service = 'dird'
 
     @classmethod
-    def get_lookup_result(self, term, profile, token=None):
+    def get_lookup_result(cls, term, profile, token=None):
         params = {'term': term}
         url = u'https://localhost:9489/0.1/directories/lookup/{profile}'
         result = requests.get(url.format(profile=profile),
@@ -58,13 +58,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def lookup(self, term, profile, token=VALID_TOKEN):
-        response = self.get_lookup_result(term, profile, token=token)
+    def lookup(cls, term, profile, token=VALID_TOKEN):
+        response = cls.get_lookup_result(term, profile, token=token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
     @classmethod
-    def get_headers_result(self, profile, token=None):
+    def get_headers_result(cls, profile, token=None):
         url = u'https://localhost:9489/0.1/directories/lookup/{profile}/headers'
         result = requests.get(url.format(profile=profile),
                               headers={'X-Auth-Token': token},
@@ -72,13 +72,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def headers(self, profile):
-        response = self.get_headers_result(profile, token=VALID_TOKEN)
+    def headers(cls, profile):
+        response = cls.get_headers_result(profile, token=VALID_TOKEN)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
     @classmethod
-    def get_reverse_result(self, exten, profile, xivo_user_uuid, token=None):
+    def get_reverse_result(cls, exten, profile, xivo_user_uuid, token=None):
         params = {'exten': exten}
         url = u'https://localhost:9489/0.1/directories/reverse/{profile}/{xivo_user_uuid}'
         result = requests.get(url.format(profile=profile, xivo_user_uuid=xivo_user_uuid),
@@ -88,13 +88,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def reverse(self, exten, profile, xivo_user_uuid, token=VALID_TOKEN):
-        response = self.get_reverse_result(exten, profile, xivo_user_uuid, token=token)
+    def reverse(cls, exten, profile, xivo_user_uuid, token=VALID_TOKEN):
+        response = cls.get_reverse_result(exten, profile, xivo_user_uuid, token=token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
     @classmethod
-    def get_favorites_result(self, profile, token=None):
+    def get_favorites_result(cls, profile, token=None):
         url = u'https://localhost:9489/0.1/directories/favorites/{profile}'
         result = requests.get(url.format(profile=profile),
                               headers={'X-Auth-Token': token},
@@ -102,13 +102,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def favorites(self, profile, token=VALID_TOKEN):
-        response = self.get_favorites_result(profile, token=token)
+    def favorites(cls, profile, token=VALID_TOKEN):
+        response = cls.get_favorites_result(profile, token=token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
     @classmethod
-    def put_favorite_result(self, directory, contact, token=None):
+    def put_favorite_result(cls, directory, contact, token=None):
         url = u'https://localhost:9489/0.1/directories/favorites/{directory}/{contact}'
         result = requests.put(url.format(directory=directory, contact=contact),
                               headers={'X-Auth-Token': token},
@@ -116,12 +116,12 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def put_favorite(self, directory, contact, token=VALID_TOKEN):
-        response = self.put_favorite_result(directory, contact, token=token)
+    def put_favorite(cls, directory, contact, token=VALID_TOKEN):
+        response = cls.put_favorite_result(directory, contact, token=token)
         assert_that(response.status_code, equal_to(204))
 
     @classmethod
-    def delete_favorite_result(self, directory, contact, token=None):
+    def delete_favorite_result(cls, directory, contact, token=None):
         url = u'https://localhost:9489/0.1/directories/favorites/{directory}/{contact}'
         result = requests.delete(url.format(directory=directory, contact=contact),
                                  headers={'X-Auth-Token': token},
@@ -129,8 +129,8 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def delete_favorite(self, directory, contact):
-        response = self.delete_favorite_result(directory, contact, token=VALID_TOKEN)
+    def delete_favorite(cls, directory, contact):
+        response = cls.delete_favorite_result(directory, contact, token=VALID_TOKEN)
         assert_that(response.status_code, equal_to(204))
 
     @classmethod
@@ -158,7 +158,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return response.json()
 
     @classmethod
-    def post_personal_result(self, personal_infos, token=None):
+    def post_personal_result(cls, personal_infos, token=None):
         url = 'https://localhost:9489/0.1/personal'
         result = requests.post(url,
                                data=json.dumps(personal_infos),
@@ -168,13 +168,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def post_personal(self, personal_infos, token=VALID_TOKEN):
-        response = self.post_personal_result(personal_infos, token)
+    def post_personal(cls, personal_infos, token=VALID_TOKEN):
+        response = cls.post_personal_result(personal_infos, token)
         assert_that(response.status_code, equal_to(201))
         return response.json()
 
     @classmethod
-    def import_personal_result(self, csv, token=None, encoding='utf-8'):
+    def import_personal_result(cls, csv, token=None, encoding='utf-8'):
         url = 'https://localhost:9489/0.1/personal/import'
         content_type = 'text/csv; charset={}'.format(encoding)
         result = requests.post(url,
@@ -185,13 +185,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def import_personal(self, personal_infos, token=VALID_TOKEN, encoding='utf-8'):
-        response = self.import_personal_result(personal_infos, token, encoding)
+    def import_personal(cls, personal_infos, token=VALID_TOKEN, encoding='utf-8'):
+        response = cls.import_personal_result(personal_infos, token, encoding)
         assert_that(response.status_code, equal_to(201))
         return response.json()
 
     @classmethod
-    def list_personal_result(self, token=None):
+    def list_personal_result(cls, token=None):
         url = 'https://localhost:9489/0.1/personal'
         result = requests.get(url,
                               headers={'X-Auth-Token': token},
@@ -199,8 +199,8 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def list_personal(self, token=VALID_TOKEN):
-        response = self.list_personal_result(token)
+    def list_personal(cls, token=VALID_TOKEN):
+        response = cls.list_personal_result(token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
@@ -213,7 +213,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return response.json()['items']
 
     @classmethod
-    def export_personal_result(self, token=None):
+    def export_personal_result(cls, token=None):
         url = 'https://localhost:9489/0.1/personal'
         result = requests.get(url,
                               headers={'X-Auth-Token': token},
@@ -222,13 +222,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def export_personal(self, token=VALID_TOKEN):
-        response = self.export_personal_result(token)
+    def export_personal(cls, token=VALID_TOKEN):
+        response = cls.export_personal_result(token)
         assert_that(response.status_code, equal_to(200))
         return response.text
 
     @classmethod
-    def get_personal_result(self, personal_id, token=None):
+    def get_personal_result(cls, personal_id, token=None):
         url = 'https://localhost:9489/0.1/personal/{contact_uuid}'
         result = requests.get(url.format(contact_uuid=personal_id),
                               headers={'X-Auth-Token': token},
@@ -236,8 +236,8 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_personal(self, personal_id, token=VALID_TOKEN):
-        response = self.get_personal_result(personal_id, token)
+    def get_personal(cls, personal_id, token=VALID_TOKEN):
+        response = cls.get_personal_result(personal_id, token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
@@ -273,7 +273,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return response.json()['items']
 
     @classmethod
-    def put_personal_result(self, personal_id, personal_infos, token=None):
+    def put_personal_result(cls, personal_id, personal_infos, token=None):
         url = 'https://localhost:9489/0.1/personal/{contact_uuid}'
         result = requests.put(url.format(contact_uuid=personal_id),
                               data=json.dumps(personal_infos),
@@ -283,13 +283,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def put_personal(self, personal_id, personal_infos, token=VALID_TOKEN):
-        response = self.put_personal_result(personal_id, personal_infos, token)
+    def put_personal(cls, personal_id, personal_infos, token=VALID_TOKEN):
+        response = cls.put_personal_result(personal_id, personal_infos, token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
     @classmethod
-    def delete_personal_result(self, personal_id, token=None):
+    def delete_personal_result(cls, personal_id, token=None):
         url = 'https://localhost:9489/0.1/personal/{contact_uuid}'
         result = requests.delete(url.format(contact_uuid=personal_id),
                                  headers={'X-Auth-Token': token},
@@ -297,8 +297,8 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def delete_personal(self, personal_id, token=VALID_TOKEN):
-        response = self.delete_personal_result(personal_id, token)
+    def delete_personal(cls, personal_id, token=VALID_TOKEN):
+        response = cls.delete_personal_result(personal_id, token)
         assert_that(response.status_code, equal_to(204))
 
     @classmethod
@@ -309,7 +309,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
                         verify=CA_CERT)
 
     @classmethod
-    def purge_personal_result(self, token=None):
+    def purge_personal_result(cls, token=None):
         url = 'https://localhost:9489/0.1/personal'
         result = requests.delete(url,
                                  headers={'X-Auth-Token': token},
@@ -317,12 +317,12 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def purge_personal(self, token=VALID_TOKEN):
-        response = self.purge_personal_result(token)
+    def purge_personal(cls, token=VALID_TOKEN):
+        response = cls.purge_personal_result(token)
         assert_that(response.status_code, equal_to(204))
 
     @classmethod
-    def get_personal_with_profile_result(self, profile, token=None):
+    def get_personal_with_profile_result(cls, profile, token=None):
         url = 'https://localhost:9489/0.1/directories/personal/{profile}'
         result = requests.get(url.format(profile=profile),
                               headers={'X-Auth-Token': token},
@@ -330,13 +330,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_personal_with_profile(self, profile, token=VALID_TOKEN):
-        response = self.get_personal_with_profile_result(profile, token)
+    def get_personal_with_profile(cls, profile, token=VALID_TOKEN):
+        response = cls.get_personal_with_profile_result(profile, token)
         assert_that(response.status_code, equal_to(200))
         return response.json()
 
     @classmethod
-    def get_menu_cisco_result(self, profile, xivo_user_uuid, proxy=None, token=None):
+    def get_menu_cisco_result(cls, profile, xivo_user_uuid, proxy=None, token=None):
         url = 'https://localhost:9489/0.1/directories/menu/{profile}/{xivo_user_uuid}/cisco'
         result = requests.get(url.format(profile=profile, xivo_user_uuid=xivo_user_uuid),
                               headers={'X-Auth-Token': token,
@@ -345,13 +345,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_menu_cisco(self, profile, xivo_user_uuid, proxy=None, token=VALID_TOKEN):
-        response = self.get_menu_cisco_result(profile, xivo_user_uuid, proxy, token)
+    def get_menu_cisco(cls, profile, xivo_user_uuid, proxy=None, token=VALID_TOKEN):
+        response = cls.get_menu_cisco_result(profile, xivo_user_uuid, proxy, token)
         assert_that(response.status_code, equal_to(200))
         return response.text
 
     @classmethod
-    def get_input_cisco_result(self, profile, xivo_user_uuid, proxy=None, token=None):
+    def get_input_cisco_result(cls, profile, xivo_user_uuid, proxy=None, token=None):
         url = 'https://localhost:9489/0.1/directories/input/{profile}/{xivo_user_uuid}/cisco'
         result = requests.get(url.format(profile=profile, xivo_user_uuid=xivo_user_uuid),
                               headers={'X-Auth-Token': token,
@@ -360,13 +360,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_input_cisco(self, profile, xivo_user_uuid, proxy=None, token=VALID_TOKEN):
-        response = self.get_input_cisco_result(profile, xivo_user_uuid, proxy, token)
+    def get_input_cisco(cls, profile, xivo_user_uuid, proxy=None, token=VALID_TOKEN):
+        response = cls.get_input_cisco_result(profile, xivo_user_uuid, proxy, token)
         assert_that(response.status_code, equal_to(200))
         return response.text
 
     @classmethod
-    def get_lookup_cisco_result(self, profile, xivo_user_uuid,
+    def get_lookup_cisco_result(cls, profile, xivo_user_uuid,
                                 proxy=None, term=None, token=None, limit=None, offset=None):
         url = 'https://localhost:9489/0.1/directories/lookup/{profile}/{xivo_user_uuid}/cisco'
         params = {'term': term, 'limit': limit, 'offset': offset}
@@ -378,13 +378,13 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_lookup_cisco(self, profile, xivo_user_uuid, term, proxy=None, token=VALID_TOKEN, limit=None, offset=None):
-        response = self.get_lookup_cisco_result(profile, xivo_user_uuid, proxy, term, token, limit, offset)
+    def get_lookup_cisco(cls, profile, xivo_user_uuid, term, proxy=None, token=VALID_TOKEN, limit=None, offset=None):
+        response = cls.get_lookup_cisco_result(profile, xivo_user_uuid, proxy, term, token, limit, offset)
         assert_that(response.status_code, equal_to(200))
         return response.text
 
     @classmethod
-    def get_input_aastra_result(self, profile, xivo_user_uuid, proxy=None, token=None):
+    def get_input_aastra_result(cls, profile, xivo_user_uuid, proxy=None, token=None):
         url = 'https://localhost:9489/0.1/directories/input/{profile}/{xivo_user_uuid}/aastra'
         result = requests.get(url.format(profile=profile, xivo_user_uuid=xivo_user_uuid),
                               headers={'X-Auth-Token': token,
@@ -393,7 +393,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_lookup_aastra_result(self, profile, xivo_user_uuid,
+    def get_lookup_aastra_result(cls, profile, xivo_user_uuid,
                                  proxy=None, term=None, token=None, limit=None, offset=None):
         url = 'https://localhost:9489/0.1/directories/lookup/{profile}/{xivo_user_uuid}/aastra'
         params = {'term': term, 'limit': limit, 'offset': offset}
@@ -405,7 +405,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_input_polycom_result(self, profile, xivo_user_uuid, proxy=None, token=None):
+    def get_input_polycom_result(cls, profile, xivo_user_uuid, proxy=None, token=None):
         url = 'https://localhost:9489/0.1/directories/input/{profile}/{xivo_user_uuid}/polycom'
         result = requests.get(url.format(profile=profile, xivo_user_uuid=xivo_user_uuid),
                               headers={'X-Auth-Token': token,
@@ -414,7 +414,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_lookup_polycom_result(self, profile, xivo_user_uuid,
+    def get_lookup_polycom_result(cls, profile, xivo_user_uuid,
                                   proxy=None, term=None, token=None, limit=None, offset=None):
         url = 'https://localhost:9489/0.1/directories/lookup/{profile}/{xivo_user_uuid}/polycom'
         params = {'term': term, 'limit': limit, 'offset': offset}
@@ -426,7 +426,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_input_snom_result(self, profile, xivo_user_uuid, proxy=None, token=None):
+    def get_input_snom_result(cls, profile, xivo_user_uuid, proxy=None, token=None):
         url = 'https://localhost:9489/0.1/directories/input/{profile}/{xivo_user_uuid}/snom'
         result = requests.get(url.format(profile=profile, xivo_user_uuid=xivo_user_uuid),
                               headers={'X-Auth-Token': token,
@@ -435,7 +435,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_lookup_snom_result(self, profile, xivo_user_uuid,
+    def get_lookup_snom_result(cls, profile, xivo_user_uuid,
                                proxy=None, term=None, token=None, limit=None, offset=None):
         url = 'https://localhost:9489/0.1/directories/lookup/{profile}/{xivo_user_uuid}/snom'
         params = {'term': term, 'limit': limit, 'offset': offset}
@@ -447,7 +447,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_lookup_thomson_result(self, profile, xivo_user_uuid,
+    def get_lookup_thomson_result(cls, profile, xivo_user_uuid,
                                   proxy=None, term=None, token=None, limit=None, offset=None):
         url = 'https://localhost:9489/0.1/directories/lookup/{profile}/{xivo_user_uuid}/thomson'
         params = {'term': term, 'limit': limit, 'offset': offset}
@@ -459,7 +459,7 @@ class BaseDirdIntegrationTest(AssetLaunchingTestCase):
         return result
 
     @classmethod
-    def get_lookup_yealink_result(self, profile, xivo_user_uuid,
+    def get_lookup_yealink_result(cls, profile, xivo_user_uuid,
                                   proxy=None, term=None, token=None, limit=None, offset=None):
         url = 'https://localhost:9489/0.1/directories/lookup/{profile}/{xivo_user_uuid}/yealink'
         params = {'term': term, 'limit': limit, 'offset': offset}
