277dc737e6b5aa3ae66c12c34970896e6525b0b8
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-10-21 08:44:52 -0400

    service discovery service: update the main config
    when a new service is started and a matching host is found in the
    xivo-dird service discovery configuration, the new source is added the
    to configured profiles and services.

---
+++ xivo_dird/plugins/service_discovery_service.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Proformatique, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from xivo_dird import BaseServicePlugin
+
+
+class ServiceDiscoveryServicePlugin(BaseServicePlugin):
+
+    def __init__(self):
+        self._service = None
+
+    def load(self, args):
+        pass
+
+
+class _Service(object):
+
+    def __init__(self):
+        pass
+
+
+class ConfigUpdater(object):
+
+    def __init__(self, config):
+        self._config = config
+        self._watched_services = {}
+        services = config['services']['service_discovery']['services']
+        self._host_configs = config['services']['service_discovery']['hosts']
+        for name, config in services.iteritems():
+            self._watched_services[name] = {
+                'lookup': self._profiles_for(config, 'lookup'),
+                'reverse': self._profiles_for(config, 'reverse'),
+                'favorites': self._profiles_for(config, 'favorites'),
+            }
+
+    def on_service_added(self, new_service_msg):
+        consul_service = new_service_msg.get('service')
+        consul_service_config = self._watched_services.get(consul_service)
+        if not consul_service_config:
+            return
+
+        host_config = self._host_configs.get(new_service_msg.get('uuid'))
+        if not host_config:
+            return
+
+        source_config = self._build_source_config(
+            consul_service_config.get('template'),
+            new_service_msg,
+            host_config)
+
+        source_name = source_config['name']
+        for dird_service, profiles in consul_service_config.iteritems():
+            dird_service_config = self._config['services'].get(dird_service)
+            if not dird_service_config:
+                continue
+
+            for profile in profiles:
+                if profile not in dird_service_config:
+                    dird_service_config[profile] = {'sources': []}
+                sources = dird_service_config[profile].get('sources') or []
+                if source_name not in sources:
+                    sources.append(source_name)
+                    dird_service_config[profile]['sources'] = sources
+
+    @staticmethod
+    def _profiles_for(config, service):
+        profiles = config.get(service, {})
+        return [profile for profile, enabled in profiles.iteritems() if enabled]
+
+    @staticmethod
+    def _build_source_config(template_filename, new_service_msg, host_config):
+        return {}
---
+++ xivo_dird/plugins/tests/test_service_discovery_service.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Proformatique, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import unittest
+
+from hamcrest import assert_that, equal_to
+from mock import Mock, patch
+
+from ..service_discovery_service import ConfigUpdater
+
+
+class TestServiceDiscoveryServicePlugin(unittest.TestCase):
+    pass
+
+
+class TestServiceDiscoveryService(unittest.TestCase):
+
+    def test_that_the_service_looks_for_remote_servers_when_starting(self):
+        pass
+
+
+class TestConfigUpdater(unittest.TestCase):
+
+    def setUp(self):
+        self.config = {
+            'services': {
+                'lookup': {
+                    'foobar': {'sources': ['source_1', 'source_2']},
+                    'default': {'sources': ['source_2']},
+                },
+                'reverse': {
+                    'foobar': {'sources': ['source_1', 'source_2']},
+                    'default': {'sources': ['source_2']},
+                },
+                'favorites': {
+                    'foobar': {'sources': ['source_2']},
+                    'default': {'sources': ['source_2']},
+                },
+                'service_discovery': {
+                    'services': {
+                        'xivo-confd': {
+                            'template': '<path/to/the/template>',
+                            'lookup': {
+                                'foobar': True,
+                                'default': True,
+                                '__switchboard': False,
+                            },
+                            'reverse': {
+                                'foobar': True,
+                                'default': False,
+                                '__switchboard': True,
+                            },
+                            'favorites': {
+                                'foobar': True,
+                                'default': True,
+                                '__switboard': False,
+                            },
+                        },
+                    },
+                    'hosts': {
+                        'ff791b0e-3d28-4b4d-bb90-2724c0a248cb': {
+                            'uuid': 'ff791b0e-3d28-4b4d-bb90-2724c0a248cb',
+                            'service_id': 'some-service-name',
+                            'service_key': 'secre7',
+                        },
+                    },
+                },
+            },
+        }
+
+    def test_that_on_service_added_modifies_the_config(self):
+        updater = ConfigUpdater(self.config)
+
+        new_service_msg = {
+            'service': 'xivo-confd',
+            'uuid': 'ff791b0e-3d28-4b4d-bb90-2724c0a248cb',
+            'port': 9487,
+            'hostname': 'remote',
+        }
+
+        source_name = 'xivo-ff791b0e-3d28-4b4d-bb90-2724c0a248cb'
+        with patch.object(updater,
+                          '_build_source_config',
+                          Mock(return_value={'name': source_name})):
+            updater.on_service_added(new_service_msg)
+
+        expected_lookup_service = {
+            'foobar': {'sources': ['source_1', 'source_2', source_name]},
+            'default': {'sources': ['source_2', source_name]}}
+        expected_reverse_service = {
+            'foobar': {'sources': ['source_1', 'source_2', source_name]},
+            'default': {'sources': ['source_2']},
+            '__switchboard': {'sources': [source_name]}}
+        expected_favorites_service = {
+            'foobar': {'sources': ['source_2', source_name]},
+            'default': {'sources': ['source_2', source_name]}}
+
+        assert_that(self.config['services']['lookup'],
+                    equal_to(expected_lookup_service))
+        assert_that(self.config['services']['reverse'],
+                    equal_to(expected_reverse_service))
+        assert_that(self.config['services']['favorites'],
+                    equal_to(expected_favorites_service))
+
+    def test_that_an_unconfigured_consul_service_does_nothing(self):
+        original_config = dict(self.config)
+
+        updater = ConfigUpdater(self.config)
+
+        new_service_msg = {
+            'service': 'xivo-ctid',
+            'uuid': 'ff791b0e-3d28-4b4d-bb90-2724c0a248cb',
+            'port': 9495,
+            'hostname': 'remote',
+        }
+
+        updater.on_service_added(new_service_msg)
+
+        assert_that(self.config, equal_to(original_config))
+
+    def test_that_a_consul_service_with_an_unknown_uuid_does_nothing(self):
+        original_config = dict(self.config)
+
+        updater = ConfigUpdater(self.config)
+
+        new_service_msg = {
+            'service': 'xivo-confd',
+            'uuid': 'ff791b0e-3d28-4b4d-bb90-2724c0a24NOT',
+            'port': 9495,
+            'hostname': 'remote',
+        }
+
+        updater.on_service_added(new_service_msg)
+
+        assert_that(self.config, equal_to(original_config))
