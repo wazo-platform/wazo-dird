aba5eade79e2abfed7e8b4cf5ffa434fca2565d4
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-03-24 08:14:34 -0400

    integration test: disable a test that is too unstable on jenkins

--- integration_tests/suite/test_personal.py
+++ integration_tests/suite/test_personal.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+import unittest
 from hamcrest import all_of
 from hamcrest import assert_that
 from hamcrest import contains
@@ -57,6 +58,9 @@ class TestListPersonal(BaseDirdIntegrationTest):
         assert_that(result['items'], contains())
 
 
+# This test will be more stable when dird gets a /status resource to know if its connected
+# to rabbitmq. Until then, this test fails most of the time on jenkins.
+@unittest.skip('Waiting for a /status resource')
 class TestDeletedUser(BaseDirdIntegrationTest):
 
     asset = 'personal_only'
