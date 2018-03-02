77bc705a26cfd457b21e66f5cf78d97d1d0620bd
Francois Blackburn <fblackburn@wazo.community>
2018-03-02 08:39:12 -0500

    remove empty __init__.py

--- integration_tests/docker/broken-plugins/xivo_dird_broken_plugins/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2015 Avencall
-# SPDX-License-Identifier: GPL-3.0+
--- xivo_dird/plugins/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2014 Avencall
-# SPDX-License-Identifier: GPL-3.0+
