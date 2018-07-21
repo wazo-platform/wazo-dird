968cf72dfd421c4fdd907749145f920851d0ec83
Francois Blackburn <fblackburn@wazo.community>
2018-07-21 08:14:08 -0400

    fix test documentation
    fetch spec before validate to control the timeout

--- integration_tests/suite/test_documentation.py
+++ integration_tests/suite/test_documentation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import requests
@@ -15,11 +15,13 @@ class TestDocumentation(BaseDirdIntegrationTest):
     asset = 'documentation'
 
     def test_documentation_errors(self):
-        api_url = 'https://dird:9489/0.1/api/api.yml'
-        self.validate_api(api_url)
+        dird_port = self.service_port(9489, 'dird')
+        api_url = 'https://localhost:{port}/0.1/api/api.yml'.format(port=dird_port)
+        api = requests.get(api_url, verify=False)
+        self.validate_api(api)
 
-    def validate_api(self, url):
+    def validate_api(self, api):
         validator_port = self.service_port(8080, 'swagger-validator')
         validator_url = u'http://localhost:{port}/debug'.format(port=validator_port)
-        response = requests.get(validator_url, params={'url': url})
+        response = requests.post(validator_url, data=api)
         assert_that(response.json(), empty(), pprint.pformat(response.json()))
