52c307346b47cec2dd1bce80fec75021688d1a0a
Pascal Cadotte Michaud <pcm@wazo.io>
2019-02-20 13:36:25 -0500

    display: add the list_ method to the queries

--- wazo_dird/database/queries/display.py
+++ wazo_dird/database/queries/display.py
@@ -1,7 +1,10 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from sqlalchemy import and_
+from sqlalchemy import (
+    and_,
+    text,
+)
 from .base import BaseDAO
 from ..import (
     Display,
@@ -32,25 +35,45 @@ class DisplayCRUD(BaseDAO):
                 Display.uuid == display_uuid,
             )).first()
 
-            columns = []
-            for column in display.columns:
-                d = {'name': column.name}
-                if column.title is not None:
-                    d['title'] = column.title
-                if column.type is not None:
-                    d['type'] = column.type
-                if column.number_display is not None:
-                    d['number_display'] = column.number_display
-                if column.default is not None:
-                    d['default'] = column.default
-                columns.append(d)
+            return self._from_db_format(display)
 
-            return {
-                'uuid': display.uuid,
-                'tenant_uuid': display.tenant_uuid,
-                'name': display.name,
-                'columns': columns,
-            }
+    def list_(self, visible_tenants, **list_params):
+        filter_ = self._list_filter(visible_tenants, **list_params)
+        with self.new_session() as s:
+            query = s.query(Display).filter(filter_)
+            query = self._paginate(query, **list_params)
+            return [self._from_db_format(row) for row in query.all()]
+
+    def _list_filter(self, visible_tenants, uuid=None, name=None, search=None, **list_params):
+        filter_ = text('true')
+        if visible_tenants is not None:
+            filter_ = and_(filter_, Display.tenant_uuid.in_(visible_tenants))
+        if uuid is not None:
+            filter_ = and_(filter_, Display.uuid == uuid)
+        if name is not None:
+            filter_ = and_(filter_, Display.name == name)
+        if search is not None:
+            pattern = '%{}%'.format(search)
+            filter_ = and_(filter_, Display.name.ilike(pattern))
+        return filter_
+
+    def _paginate(self, query, limit=None, offset=None, order=None, direction=None, **ignored):
+        if order and direction:
+            field = None
+            if order == 'name':
+                field = Display.name
+
+            if field:
+                order_clause = field.asc() if direction == 'asc' else field.desc()
+                query = query.order_by(order_clause)
+
+        if limit is not None:
+            query = query.limit(limit)
+
+        if offset is not None:
+            query = query.offset(offset)
+
+        return query
 
     @staticmethod
     def _add_column(session, **column_body):
@@ -63,3 +86,25 @@ class DisplayCRUD(BaseDAO):
         session.add(display)
         session.flush()
         return display.uuid
+
+    @staticmethod
+    def _from_db_format(display):
+        columns = []
+        for column in display.columns:
+            d = {'field': column.field}
+            if column.title is not None:
+                d['title'] = column.title
+            if column.type is not None:
+                d['type'] = column.type
+            if column.number_display is not None:
+                d['number_display'] = column.number_display
+            if column.default is not None:
+                d['default'] = column.default
+            columns.append(d)
+
+        return {
+            'uuid': display.uuid,
+            'tenant_uuid': display.tenant_uuid,
+            'name': display.name,
+            'columns': columns,
+        }
