5cfd7f22783ee1abbd2d25f91e13d5b1f197badb
Pascal Cadotte Michaud <pcm@wazo.io>
2019-03-01 15:22:47 -0500

    display: auto-create a default display when a tenant is created

--- wazo_dird/plugins/display_service/plugin.py
+++ wazo_dird/plugins/display_service/plugin.py
@@ -1,22 +1,31 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import logging
+import kombu
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+
+from xivo_bus.marshaler import InvalidMessage, Marshaler
+from xivo_bus.resources.auth.events import TenantCreatedEvent
 
 from wazo_dird import (
     BaseServicePlugin,
     database,
 )
 
+logger = logging.getLogger(__name__)
+
 
 class DisplayServicePlugin(BaseServicePlugin):
 
     def load(self, dependencies):
-        self._config = dependencies['config']
-        db_uri = self._config['db_uri']
+        config = dependencies['config']
+        bus = dependencies['bus']
+        db_uri = config['db_uri']
         Session = self._new_db_session(db_uri)
-        return _DisplayService(database.DisplayCRUD(Session))
+        return _DisplayService(database.DisplayCRUD(Session), bus)
 
     def _new_db_session(self, db_uri):
         self._Session = scoped_session(sessionmaker())
@@ -27,8 +36,51 @@ class DisplayServicePlugin(BaseServicePlugin):
 
 class _DisplayService:
 
-    def __init__(self, crud):
+    QUEUE = kombu.Queue(
+        exchange=kombu.Exchange('xivo', type='topic'),
+        routing_key='auth.tenants.*.created',
+        exclusive=True,
+    )
+    _default_display_columns = [
+        {'field': 'name', 'title': 'Nom', 'type': 'name'},
+        {'field': 'phone', 'title': "Num\xE9ro", 'type': 'number', 'number_display': '{name}'},
+        {'field': 'phone_mobile', 'title': 'Mobile', 'type': 'number', 'number_display': '{name} (mobile)'},
+        {'field': 'voicemail', 'title': "Bo\xEEte vocale", 'type': 'voicemail'},
+        {'field': 'favorite', 'title': 'Favoris', 'type': 'favorite'},
+        {'field': 'email', 'title': 'E-mail', 'type': 'email'},
+    ]
+
+    def __init__(self, crud, bus):
         self._display_crud = crud
+        bus.add_consumer(self.QUEUE, self._on_new_tenant)
+
+    def create(self, **body):
+        return self._display_crud.create(**body)
 
     def list_(self, visible_tenants, **list_params):
         return self._display_crud.list_(visible_tenants, **list_params)
+
+    def _auto_create_display(self, tenant_uuid, name):
+        try:
+            display = self.create(
+                tenant_uuid=tenant_uuid,
+                name='auto_{}'.format(name),
+                columns=self._default_display_columns,
+            )
+            logger.info(
+                'display %s auto created for tenant %s',
+                display['uuid'], display['tenant_uuid'],
+            )
+        except Exception as e:
+            logger.info('auto display creation failed %s', e)
+
+    def _on_new_tenant(self, body, message):
+        try:
+            event = Marshaler.unmarshal_message(body, TenantCreatedEvent)
+            body = event.marshal()
+        except (InvalidMessage, KeyError):
+            logger.info('Ignoring the following malformed bus message: %s', body)
+        else:
+            self._auto_create_display(body['uuid'], body['name'])
+        finally:
+            message.ack()
