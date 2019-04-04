1ede4b2e699a16198826ecab967ec3ff55d8d144
Genevieve Gilbert <ggilbert@wazo.io>
2019-04-04 15:06:32 -0400

    lookup sources: add load unit test

---
+++ wazo_dird/plugins/lookup_sources/tests/test_source_plugin.py
@@ -0,0 +1,27 @@
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from unittest import TestCase
+from mock import Mock
+
+from hamcrest import assert_that, calling, not_, raises
+
+from ..plugin import SourceViewPlugin
+
+
+class TestSourcePlugin(TestCase):
+
+    DEPENDENCIES = {
+        'api': Mock(),
+        'source_service': Mock(),
+    }
+
+    def setUp(self):
+        self.source = SourceViewPlugin()
+
+    def test_load(self):
+        self.source.load(self.DEPENDENCIES)
+        assert_that(
+            calling(self.source.load).with_args(self.DEPENDENCIES),
+            not_(raises(Exception))
+        )
