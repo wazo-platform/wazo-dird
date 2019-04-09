5a3ba6ab451a8afdc75a428e7488f3b8fbf06741
Pascal Cadotte Michaud <pcm@wazo.io>
2019-04-09 08:18:03 -0400

    wazo contact: read the key_file to initialize the auth client

--- wazo_dird/plugins/wazo_user_backend/contact.py
+++ wazo_dird/plugins/wazo_user_backend/contact.py
@@ -1,6 +1,7 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from xivo.config_helper import parse_config_file
 from xivo_auth_client import Client as AuthClient
 from xivo_confd_client import Client as ConfdClient
 
@@ -20,6 +21,12 @@ class ContactLister:
 
     @classmethod
     def from_config(cls, source_config):
+        auth_config = source_config['auth']
+        if auth_config.get('key_file'):
+            # File must be readable by wazo-dird
+            key_file = parse_config_file(auth_config.pop('key_file'))
+            auth_config['username'] = key_file['service_id']
+            auth_config['password'] = key_file['service_key']
         auth = AuthClient(**source_config['auth'])
         token = auth.token.new(backend='wazo_user', expiration='60')['token']
         confd = ConfdClient(token=token, **source_config['confd'])
