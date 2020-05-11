8adf82ff50eb158626032a9526abebd6aaa70d06
Francois Blackburn <fblackburn@wazo.community>
2020-05-11 07:42:34 -0400

    remove https self_check for service_discovery

--- wazo_dird/service_discovery.py
+++ wazo_dird/service_discovery.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import requests
@@ -6,11 +6,9 @@ import requests
 
 # this function is not executed from the main thread
 def self_check(port):
-    url = 'https://localhost:{}/0.1/directories/lookup/foobar/headers'.format(port)
+    url = 'http://localhost:{}/0.1/status'.format(port)
     try:
-        response = requests.get(
-            url, headers={'accept': 'application/json'}, verify=False
-        )
+        response = requests.get(url, headers={'accept': 'application/json'})
         return response.status_code == 401
     except Exception:
         return False
