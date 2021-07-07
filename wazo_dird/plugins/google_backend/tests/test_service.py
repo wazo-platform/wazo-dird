e6c5714e63b64d3e1bac63ee637b5754090a8a08
Ben Boyle <me@benboyle.ca>
2021-07-07 14:28:29 -0300

    rewrite tests to refer to shared contact response

--- wazo_dird/plugins/google_backend/tests/test_service.py
+++ wazo_dird/plugins/google_backend/tests/test_service.py
@@ -121,163 +121,85 @@ class TestGoogleContactFormatter(unittest.TestCase):
         self.formatter = services.ContactFormatter()
 
     def test_format_id(self):
-        google_contact = {
-            'id': {
-                '$t': 'http://www.google.com/m8/feeds/contacts/me%40example.com/base/72b6b4840bf772e6'
-            }
-        }
-
-        formatted_contact = self.formatter.format(google_contact)
-
-        assert_that(formatted_contact, has_entries(id='72b6b4840bf772e6'))
+        formatted_contact = self.formatter.format(self.google_contact)
+        assert_that(formatted_contact, has_entries(id='3ef7a14009000bb7'))
 
     def test_format_name(self):
-        google_contact = {'title': {'$t': 'Joe Blow', 'type': 'text'}}
-
-        formatted_contact = self.formatter.format(google_contact)
-
-        assert_that(formatted_contact, has_entries(name='Joe Blow'))
-
-    def test_format_name_when_fullname(self):
-        google_contact = {
-            'title': {'$t': 'Test User1', 'type': 'text'},
-            'gd$name': {'gd$fullName': {'$t': 'Test FullName'}},
-        }
-
-        formatted_contact = self.formatter.format(google_contact)
-        assert_that(formatted_contact, has_entries(name='Test FullName'))
+        formatted_contact = self.formatter.format(self.google_contact)
+        assert_that(formatted_contact, has_entries(name='Luigi Bros'))
 
     def test_format_first_name(self):
-        google_contact = {'gd$name': {'gd$givenName': {'$t': 'Test'}}}
-
-        formatted_contact = self.formatter.format(google_contact)
-        assert_that(formatted_contact, has_entries(firstname='Test'))
+        formatted_contact = self.formatter.format(self.google_contact)
+        assert_that(formatted_contact, has_entries(firstname='Luigi'))
 
     def test_format_last_name(self):
-        google_contact = {'gd$name': {'gd$familyName': {'$t': 'Family'}}}
-
-        formatted_contact = self.formatter.format(google_contact)
-        assert_that(formatted_contact, has_entries(lastname='Family'))
+        formatted_contact = self.formatter.format(self.google_contact)
+        assert_that(formatted_contact, has_entries(lastname='Bros'))
 
     def test_multiple_numbers(self):
-        google_contact = {
-            'gd$phoneNumber': [
-                {
-                    'rel': 'http://schemas.google.com/g/2005#mobile',
-                    'uri': 'tel:+1-555-123-4567',
-                    '$t': '+1 555-123-4567',
-                },
-                {
-                    'rel': 'http://schemas.google.com/g/2005#home',
-                    'uri': 'tel:+1-555-123-9876',
-                    '$t': '+1 5551239876',
-                },
-                {
-                    'label': 'custom',
-                    '$t': '(555) 123-1111',
-                    'uri': 'tel:+1-555-123-1111',
-                },
-            ]
-        }
-
-        formatted_contact = self.formatter.format(google_contact)
+        formatted_contact = self.formatter.format(self.google_contact)
 
         assert_that(
             formatted_contact,
             has_entries(
                 numbers_by_label=has_entries(
-                    mobile='+15551234567', home='+15551239876', custom='5551231111'
+                    mobile='5555558888', home='5555554321', work='5555551234'
                 ),
-                numbers=contains_inanyorder(
-                    '+15551239876', '5551231111', '+15551234567'
-                ),
+                numbers=contains_inanyorder('5555558888', '5555554321', '5555551234'),
                 numbers_except_label=has_entries(
-                    mobile=has_items('+15551239876', '5551231111'),
-                    home=has_items('+15551234567', '5551231111'),
+                    mobile=has_items('5555554321', '5555551234'),
+                    home=has_items('5555558888', '5555551234'),
                 ),
             ),
         )
 
     def test_multiple_emails(self):
-        google_contact = {
-            'gd$email': [
-                {
-                    'address': 'home@example.com',
-                    'rel': 'http://schemas.google.com/g/2005#home',
-                },
-                {'address': 'other@example.com', 'label': 'custom'},
-                {'address': 'other2@example.com'},
-            ]
-        }
-
-        formatted_contact = self.formatter.format(google_contact)
+        formatted_contact = self.formatter.format(self.google_contact)
 
         assert_that(
             formatted_contact,
             has_entries(
                 emails=contains(
-                    has_entries(address='home@example.com', label='home'),
-                    has_entries(address='other@example.com', label='custom'),
-                    has_entries(address='other2@example.com', label=''),
+                    has_entries(address='luigi_bros@caramail.com', label='Old'),
+                    has_entries(address='luigi2@example.com', label='New'),
                 ),
             ),
         )
 
     def test_organization(self):
-        google_contact = {
-            'gd$organization': [
-                {
-                    'gd$orgTitle': {'$t': 'Tester'},
-                    'gd$orgName': {'$t': 'Test Company'},
-                },
-                {'gd$orgTitle': {'$t': 'President'}, 'gd$orgName': {'$t': 'Acme'}},
-            ],
-        }
-
-        formatted_contact = self.formatter.format(google_contact)
+        formatted_contact = self.formatter.format(self.google_contact)
 
         assert_that(
             formatted_contact,
             has_entries(
                 organizations=contains(
-                    has_entries(name='Test Company', title='Tester'),
-                    has_entries(name='Acme', title='President'),
+                    has_entries(name='Mushroom Kingdom', title='Plumber'),
                 ),
             ),
         )
 
     def test_addresses(self):
-        google_contact = {
-            'gd$structuredPostalAddress': [
-                {
-                    'rel': 'http://schemas.google.com/g/2005#home',
-                    'gd$formattedAddress': {'$t': 'First address'},
-                },
-                {
-                    'label': 'Test address',
-                    'gd$formattedAddress': {'$t': 'Second address'},
-                },
-            ],
-        }
-
-        formatted_contact = self.formatter.format(google_contact)
+        formatted_contact = self.formatter.format(self.google_contact)
 
         assert_that(
             formatted_contact,
             has_entries(
                 addresses=contains(
-                    has_entries(address='First address', label='home'),
-                    has_entries(address='Second address', label='Test address'),
+                    has_entries(
+                        address='1600 Pennsylvania Avenue NW\\nWashington, DC 20500\\nUS',
+                        label='work',
+                    ),
+                    has_entries(
+                        address='24 Sussex Dr\\nOttawa, ON K1M 1M4\\nCA', label='home'
+                    ),
                 ),
             ),
         )
 
     def test_note(self):
-        google_contact = {'content': {'$t': 'Notey'}}
-
-        formatted_contact = self.formatter.format(google_contact)
+        formatted_contact = self.formatter.format(self.google_contact)
 
         assert_that(
             formatted_contact,
-            has_entries(note='Notey'),
+            has_entries(note='Notes user 02'),
         )
