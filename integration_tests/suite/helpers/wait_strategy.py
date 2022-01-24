898573091cff3547602c9f6a00208a239daf7165
Francois Blackburn <fblackburn@wazo.community>
2022-01-24 15:51:57 -0500

    fix linter issue

--- integration_tests/suite/helpers/wait_strategy.py
+++ integration_tests/suite/helpers/wait_strategy.py
@@ -8,9 +8,7 @@ from hamcrest import assert_that, has_entries
 from wazo_test_helpers import until
 from wazo_test_helpers.wait_strategy import WaitStrategy, NoWaitStrategy
 
-__all__ = [
-    'NoWaitStrategy'
-]
+__all__ = ['NoWaitStrategy']
 
 
 class RestApiOkWaitStrategy(WaitStrategy):
