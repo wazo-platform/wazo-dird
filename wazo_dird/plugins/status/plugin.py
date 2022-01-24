341a8e90e3f082c21b5e70f16174162f4e2d290e
Francois Blackburn <fblackburn@wazo.community>
2022-01-24 14:39:39 -0500

    status: add rest_api to /status
    why: to be used by integration tests

--- wazo_dird/plugins/status/plugin.py
+++ wazo_dird/plugins/status/plugin.py
@@ -1,7 +1,8 @@
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_dird import BaseViewPlugin
+from xivo.status import Status
 
 from .http import StatusResource
 
@@ -15,6 +16,12 @@ class StatusViewPlugin(BaseViewPlugin):
 
         status_aggregator = dependencies['status_aggregator']
 
+        status_aggregator.add_provider(provide_status)
+
         api.add_resource(
             StatusResource, '/status', resource_class_args=[status_aggregator]
         )
+
+
+def provide_status(status):
+    status['rest_api']['status'] = Status.ok
