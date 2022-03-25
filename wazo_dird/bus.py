d258f049eb1bb757d5c3b56cc87a97beffa9aae3
Julien Alie <jalie@wazo.io>
2022-03-25 10:09:20 -0400

    Adds enabled to bus
    Why:
    
    * The bus needs to the ability to be enabled/disabled for certain
    integration tests

--- wazo_dird/bus.py
+++ wazo_dird/bus.py
@@ -7,5 +7,39 @@ from xivo_bus.publisher import BusPublisher
 
 
 class CoreBus(BusPublisher, BusConsumer):
+    def __init__(
+        self,
+        service_uuid,
+        enabled=True,
+        username='guest',
+        password='guest',
+        host='localhost',
+        port=5672,
+        exchange_name='',
+        exchange_type='',
+        **kwargs
+    ):
+        super().__init__(
+            service_uuid=service_uuid,
+            username=username,
+            password=password,
+            host=host,
+            port=port,
+            exchange_name=exchange_name,
+            exchange_type=exchange_type,
+            **kwargs
+        )
+        self.enabled = enabled
+
     def provide_status(self, status):
         status['bus_consumer']['status'] = Status.ok if self.is_running else Status.fail
+
+    def publish(self, event, headers=None, routing_key=None, payload=None):
+        if not self.enabled:
+            return
+        super().publish(event, headers, routing_key, payload)
+
+    def start(self):
+        if not self.enabled:
+            return
+        super().start()
