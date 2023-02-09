89e6d262cf21720e71f73d48b9fd42b7d6bb1099
Jesse Sopel <jesse.sopel@wazo.io>
2023-02-09 14:54:08 -0500

    Use Python 3.9, new workers, and Bullseye dependencies

--- integration_tests/assets/scripts/asset.csv_ws_utf8_with_pipes_with_ssl.ws.py
+++ integration_tests/assets/scripts/asset.csv_ws_utf8_with_pipes_with_ssl.ws.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
+#!/usr/bin/env python3
+# Copyright 2015-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-from __future__ import unicode_literals
-
 import sys
 
 from flask import Flask, request, Response
@@ -27,8 +24,8 @@ separator = sys.argv[1]
 charset = sys.argv[2]
 
 
-def line(fields, separator=separator):
-    return '{}\n'.format(separator.join(map(unicode, fields))).encode(charset)
+def line(fields, sep=separator):
+    return f'{sep.join(map(str, fields))}\n'.encode(charset)
 
 
 @app.route('/ws')
@@ -38,7 +35,7 @@ def ws():
     if not request.args.keys():
         result = entries
 
-    for field, term in request.args.iteritems():
+    for field, term in request.args.items():
         if field not in headers:
             continue
 
@@ -56,7 +53,7 @@ def ws():
         for entry in data:
             yield line(entry)
 
-    return Response(generate(), content_type='text/csv; charset={}'.format(charset))
+    return Response(generate(), content_type=f'text/csv; charset={charset}')
 
 
 def main():
--- wazo_dird/http_server.py
+++ wazo_dird/http_server.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
@@ -10,7 +10,7 @@ from cheroot import wsgi
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from werkzeug.contrib.fixers import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 from xivo import http_helpers
 from xivo.http_helpers import ReverseProxied
 
@@ -36,7 +36,7 @@ VERSION = 0.1
 TEMPLATE_FOLDER = 'plugins/templates'
 
 app = Flask('wazo-dird')
-api = Api(app, prefix='/{}'.format(VERSION))
+api = Api(app, prefix=f'/{VERSION}')
 
 
 class CoreRestApi:
