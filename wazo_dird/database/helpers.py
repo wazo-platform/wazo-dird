e111487e1386c3dd042c4748321f730e384b1464
Sebastien Duthil <sduthil@wazo.community>
2023-05-09 11:05:28 -0400

    config: parameterize max number of concurrent requests

--- wazo_dird/database/helpers.py
+++ wazo_dird/database/helpers.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from sqlalchemy import create_engine
@@ -7,6 +7,6 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 Session = scoped_session(sessionmaker())
 
 
-def init_db(db_uri, echo=False):
-    engine = create_engine(db_uri, echo=echo, pool_pre_ping=True)
+def init_db(db_uri, echo=False, pool_size=16):
+    engine = create_engine(db_uri, echo=echo, pool_size=pool_size, pool_pre_ping=True)
     Session.configure(bind=engine)
