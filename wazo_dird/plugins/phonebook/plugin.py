d252121767cafd03799838fb57c569d9ba36505e
Charles Langlois <charles.langlois@wazo.io>
2023-07-05 15:47:07 -0400

    phonebook.plugins: type annotations

--- wazo_dird/plugins/phonebook/plugin.py
+++ wazo_dird/plugins/phonebook/plugin.py
@@ -1,19 +1,34 @@
 # Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
+
+from typing import TypedDict
+
+from flask_restful import Api
 
 from wazo_dird import BaseViewPlugin
+from wazo_dird.plugins.phonebook_service.plugin import _PhonebookService
 
 from .http import (
+    PhonebookAll,
     PhonebookContactAll,
     PhonebookContactImport,
     PhonebookContactOne,
-    PhonebookAll,
     PhonebookOne,
 )
 
 
+class Services(TypedDict):
+    phonebook: _PhonebookService
+
+
+class Dependencies(TypedDict):
+    api: Api
+    services: Services
+
+
 class PhonebookViewPlugin(BaseViewPlugin):
-    def load(self, dependencies=None):
+    def load(self, dependencies: Dependencies):
         api = dependencies['api']
         args = (dependencies['services'].get('phonebook'),)
 
