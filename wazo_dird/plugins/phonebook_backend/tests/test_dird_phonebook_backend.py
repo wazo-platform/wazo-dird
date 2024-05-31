46a8919eace54bb78c8b4aef24e73d2a7d1e4d2d
Charles Langlois <charles.langlois@wazo.io>
2024-05-31 16:41:55 -0400

    phonebook_backend: unit tests coverage for different number formats

--- wazo_dird/plugins/phonebook_backend/tests/test_dird_phonebook_backend.py
+++ wazo_dird/plugins/phonebook_backend/tests/test_dird_phonebook_backend.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest
@@ -58,19 +58,112 @@ class TestDirdPhonebook(unittest.TestCase):
 
         assert_that(id_, equal_to({'uuid': 'some-other-uuid'}))
 
+
+class TestPhonebookBackendPluginFindFirst(unittest.TestCase):
+    def setUp(self):
+        self.source = PhonebookPlugin()
+        with patch(f'{PhonebookPlugin.__module__}.Session'):
+            with patch(
+                f'{PhonebookPlugin.__module__}.database.PhonebookContactSearchEngine'
+            ):
+                with patch(f'{PhonebookPlugin.__module__}.database.PhonebookCRUD'):
+                    self.source.load(
+                        {
+                            'config': {
+                                'name': 'test-load',
+                                'tenant_uuid': '123',
+                                'first_matched_columns': ['number'],
+                                'searched_columns': ['firstname', 'lastname'],
+                                'phonebook_uuid': '456',
+                            }
+                        }
+                    )
+                    self.engine = self.source._search_engine
+                    self.SourceResult = self.source._SourceResult
+
     def test_that_find_first_returns_a_formated_result(self):
         raw_result = self.engine.find_first_contact.return_value = {
             'id': "42",
             'name': 'foobar',
+            'number': 'example',
         }
-
-        result = self.source.first_match(s.term)
+        term = 'example'
+        result = self.source.first_match(term)
 
         assert_that(result, equal_to(self.SourceResult(raw_result)))
 
     def test_that_find_first_returns_none_when_empty_result(self):
         self.engine.find_first_contact.return_value = None
 
-        result = self.source.first_match(s.term)
+        term = 'example'
+        result = self.source.first_match(term)
 
         assert_that(result, equal_to(None))
+
+    def test_that_find_first_can_match_international_number_missing_plus_sign(self):
+        raw_result = {'id': "42", 'name': 'foobar', 'number': '+12345678910'}
+        self.engine.find_first_contact.side_effect = lambda term: (
+            raw_result if term == '+12345678910' else None
+        )
+
+        term = '12345678910'
+        result = self.source.first_match(term)
+
+        assert_that(result, equal_to(self.SourceResult(raw_result)))
+
+    def test_that_find_first_can_match_formatted_input(self):
+        raw_result = {'id': "42", 'name': 'foobar', 'number': '+12345678900'}
+        self.engine.find_first_contact.side_effect = lambda term: (
+            raw_result if term == '+12345678900' else None
+        )
+
+        term = '+1 (234) 567-8900'
+        result = self.source.first_match(term)
+
+        assert_that(result, equal_to(self.SourceResult(raw_result)))
+
+    def test_that_find_first_can_match_missing_country_code(self):
+        raw_result = {'id': "42", 'name': 'foobar', 'number': '+12345678900'}
+        self.engine.find_first_contact.side_effect = lambda term: (
+            raw_result if term == '+12345678900' else None
+        )
+
+        term = '2345678900'
+        result = self.source.first_match(term)
+
+        assert_that(result, equal_to(self.SourceResult(raw_result)))
+
+    def test_that_find_first_can_match_formatted_input_missing_country_code(self):
+        raw_result = {'id': "42", 'name': 'foobar', 'number': '+12345678900'}
+        self.engine.find_first_contact.side_effect = lambda term: (
+            raw_result if term == '+12345678900' else None
+        )
+
+        term = '(234) 567-8900'
+        result = self.source.first_match(term)
+
+        assert_that(result, equal_to(self.SourceResult(raw_result)))
+
+    def test_that_find_first_can_match_short_number_missing_country_code_and_area_code(
+        self,
+    ):
+        raw_result = {'id': "42", 'name': 'foobar', 'number': '567-8900'}
+        self.engine.find_first_contact.side_effect = lambda term: (
+            raw_result if term == '567-8900' else None
+        )
+
+        term = '5678900'
+        result = self.source.first_match(term)
+
+        assert_that(result, equal_to(self.SourceResult(raw_result)))
+
+    def test_that_find_first_can_match_short_form_no_formatting(self):
+        raw_result = {'id': "42", 'name': 'foobar', 'number': '2345678910'}
+        self.engine.find_first_contact.side_effect = lambda term: (
+            raw_result if term == '2345678910' else None
+        )
+
+        term = '12345678910'
+        result = self.source.first_match(term)
+
+        assert_that(result, equal_to(self.SourceResult(raw_result)))
