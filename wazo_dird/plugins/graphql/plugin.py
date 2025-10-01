fd47b8acd24aabc47bd6fe8c521fe8cdb5d54896
Charles Langlois <charles.langlois@wazo.io>
2025-10-01 11:29:15 -0400

    pyupgrade 3.9 -> 3.11

--- wazo_dird/plugins/graphql/plugin.py
+++ wazo_dird/plugins/graphql/plugin.py
@@ -3,7 +3,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable
+from collections.abc import Callable
+from typing import TYPE_CHECKING, Any
 
 import requests
 from graphql_server.flask import GraphQLView
