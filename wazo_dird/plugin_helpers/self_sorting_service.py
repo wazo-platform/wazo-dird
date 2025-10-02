11c42235d71033170443516d6132a2223c64133f
Charles Langlois <charles.langlois@wazo.io>
2025-10-02 17:13:10 -0400

    typing: type plugin_helpers
    extend mypy scope
    and explicitly ignore untyped imports

--- wazo_dird/plugin_helpers/self_sorting_service.py
+++ wazo_dird/plugin_helpers/self_sorting_service.py
@@ -1,21 +1,28 @@
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
 
 from sys import maxunicode
+from typing import Any
 
-MAX_CHAR = chr(maxunicode)
-ALMOST_LAST_STRING = MAX_CHAR * 16
+MAX_CHAR: str = chr(maxunicode)
+ALMOST_LAST_STRING: str = MAX_CHAR * 16
 
 
 class SelfSortingServiceMixin:
     @staticmethod
-    def sort(contacts, order=None, direction=None, **_):
+    def sort(
+        contacts: list[dict],
+        order: str | None = None,
+        direction: str | None = None,
+        **_: Any,  # TODO: remove this parameter
+    ) -> list[dict]:
         if not order:
             return contacts
 
-        reverse = direction == 'desc'
+        reverse = direction == "desc"
 
-        def get_value(contact):
+        def get_value(contact: dict) -> str:
             value = contact.get(order)
             return value or ALMOST_LAST_STRING
 
--- wazo_dird/plugin_helpers/tenant.py
+++ wazo_dird/plugin_helpers/tenant.py
@@ -1,10 +1,10 @@
-# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2022-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo.tenant_flask_helpers import Tenant, token
 
 
-def get_tenant_uuids(recurse=False):
+def get_tenant_uuids(recurse: bool = False) -> list[str]:
     tenant_uuid = Tenant.autodetect().uuid
     if not recurse:
         return [tenant_uuid]
