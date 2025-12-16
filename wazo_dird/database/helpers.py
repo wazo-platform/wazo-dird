33010f406f97ebcfa1c6541406aa03c09c8a5d20
Charles Langlois <charles.langlois@wazo.io>
2025-12-16 09:54:53 -0500

    database.helpers: return engine from init_db
    for use in integration tests

--- wazo_dird/database/helpers.py
+++ wazo_dird/database/helpers.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from sqlalchemy import create_engine
@@ -10,3 +10,4 @@ Session = scoped_session(sessionmaker())
 def init_db(db_uri, echo=False, pool_size=16):
     engine = create_engine(db_uri, echo=echo, pool_size=pool_size, pool_pre_ping=True)
     Session.configure(bind=engine)
+    return engine
