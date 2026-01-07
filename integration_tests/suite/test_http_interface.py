cb0f98e4f7aa6b2b4613b9faf5a8c967f926241a
Remy Bienvenu <remy.bienvenu@wazo.io>
2026-01-07 09:23:38 -0800

    tests: Refactor http interface test
    WAZO-4352

--- integration_tests/suite/test_http_interface.py
+++ integration_tests/suite/test_http_interface.py
@@ -10,25 +10,34 @@ from .helpers.constants import VALID_TOKEN_MAIN_TENANT
 class TestEmptyBodyReturns400(BaseDirdIntegrationTest):
     asset = 'all_routes'
 
-    def test_that_empty_body_returns_400(self):
+    def _make_http_call(self, verb: str, url: str, body: str | None):
         headers = {
             'X-Auth-Token': VALID_TOKEN_MAIN_TENANT,
         }
 
-        response = requests.post(self.url('displays'), headers=headers, data='')
-        assert response.status_code == 400
+        match verb:
+            case 'post':
+                call = requests.post
+            case 'patch':
+                call = requests.patch  # type: ignore
 
-        response = requests.post(self.url('displays'), headers=headers, data=None)
-        assert response.status_code == 400
+        return call(
+            url,
+            headers=headers,
+            data=body,
+            verify=False,
+        )
 
-        response = requests.post(self.url('profiles'), headers=headers, data='')
-        assert response.status_code == 400
+    def test_that_empty_body_returns_400(self):
+        urls = [
+            ('post', self.url('displays')),
+            ('post', self.url('profiles')),
+            ('patch', self.url('config')),
+        ]
 
-        response = requests.post(self.url('profiles'), headers=headers, data=None)
-        assert response.status_code == 400
+        for url in urls:
+            response = self._make_http_call(url[0], url[1], '')
+            assert response.status_code == 400, f'Error for url: {url}'
 
-        response = requests.patch(self.url('config'), headers=headers, data='')
-        assert response.status_code == 400
-
-        response = requests.patch(self.url('config'), headers=headers, data=None)
-        assert response.status_code == 400
+            response = self._make_http_call(url[0], url[1], None)
+            assert response.status_code == 400, f'Error for url: {url}'
