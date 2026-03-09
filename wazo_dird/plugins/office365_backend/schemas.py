6105c90268682d1e1bc2fd167505d65e6f20245e
Francois Blackburn <fblackburn@wazo.community>
2026-03-09 11:02:52 -0400

    microsoft: sort specific contacts columns by case insensitive

--- wazo_dird/plugins/office365_backend/schemas.py
+++ wazo_dird/plugins/office365_backend/schemas.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo.mallow import fields
@@ -30,6 +30,7 @@ class ListSchema(_ListSchema):
 class ContactListSchema(_ListSchema):
     searchable_columns = ['displayName', 'givenName', 'surname']
     sort_columns = ['displayName', 'givenName', 'surname']
+    sort_insensitive_columns = ['displayName', 'givenName', 'surname']
     default_sort_column = 'displayName'
 
 
