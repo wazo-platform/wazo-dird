c33a9b2c4575fcd2c5158fbf12c2da49afbd295f
Charles Langlois <charles.langlois@wazo.io>
2026-06-11 17:46:35 -0400

    fix(stress-test): revert to status_code check; ResponseContextManager has no .error

--- contribs/stress_test_graphql_reverse/locustfile.py
+++ contribs/stress_test_graphql_reverse/locustfile.py
@@ -79,8 +79,8 @@ class GraphQLReverseLookupUser(FastHttpUser):
             json={'query': query},
             catch_response=True,
         ) as response:
-            if response.error:
-                response.failure(response.error)
+            if not response.status_code:
+                response.failure('timeout or connection error')
             elif response.status_code >= 500:
                 response.failure(response.text)
             elif response.status_code >= 400:
