c4dbf727a91cb6ec2451781179fc204079923602
Charles Langlois <charles.langlois@wazo.io>
2026-06-11 19:34:38 -0400

    reverse & phonebook logging

--- wazo_dird/plugins/phonebook_backend/plugin.py
+++ wazo_dird/plugins/phonebook_backend/plugin.py
@@ -143,9 +143,14 @@ class PhonebookPlugin(BaseSourcePlugin):
         else:
             return None
 
-    def match_all(self, extens: list[str], args: dict[str, Any] | None = None) -> dict[str, SourceResult]:
+    def match_all(
+        self, extens: list[str], args: dict[str, Any] | None = None
+    ) -> dict[str, SourceResult]:
         logger.debug('Batch matching phonebook contacts for %d extens', len(extens))
         contacts = self._search_engine.find_contacts_for_extens(extens)
+        logger.debug(
+            'matched %d phonebook contacts for %d extens', len(contacts), len(extens)
+        )
         return {
             exten: self.format_contacts([contact])[0]
             for exten, contact in contacts.items()
